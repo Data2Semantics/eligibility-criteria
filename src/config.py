9e6ca01634f95c79837ea7f2880b36cde31538fe
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-07-17 16:18:51 +0200

    Added stuff for running on server

--- src/config.py
+++ src/config.py
@@ -4,3 +4,6 @@
 SPARQL_ENDPOINT = "http://semweb.cs.vu.nl:8080/openrdf-sesame/repositories/ct"
 
 TRIAL_OUTPUT_PATH = "edited-trials"
+
+#APPLICATION_ROOT = '/eligibility'
+#SERVER_NAME = 'semweb.cs.vu.nl'
